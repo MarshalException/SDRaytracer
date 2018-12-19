ee9066e6e2949b98d3219d4e34eb9e2f42b2ef4e
MarshalException <42607449+MarshalException@users.noreply.github.com>
2018-12-19 12:05:45 +0100

    removed code smells

--- src/Cube.java
+++ src/Cube.java
@@ -2,6 +2,8 @@ import java.util.List;
 
 class Cube
 {
+	Cube(){}
+	
     public static void addCube(List<Triangle> triangles, int x, int y, int z, int w, int h, int d, RGB c, float sh)
     {  //front
         triangles.add(new Triangle(new Vec3D(x,y,z), new Vec3D(x+w,y,z), new Vec3D(x,y+h,z), c, sh));
